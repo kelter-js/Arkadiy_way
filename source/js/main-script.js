8a40238ceb57ab9c19588421508658b21c236f33
kelter-js <spawnkillerqw@gmail.com>
2021-04-08 21:57:52 +0400

    Реализовал наполовину шапку таблетки

---
+++ source/js/main-script.js
@@ -0,0 +1,15 @@
+const NAVIGATION_ELEMENT = document.querySelector('.main-nav');
+const TOGGLE_NAVIGATION_BUTTON = document.querySelector('.main-nav__toggle');
+
+NAVIGATION_ELEMENT.classList.remove('main-nav--nojs');
+
+const onClickToggleNavigation = () => {
+  if (NAVIGATION_ELEMENT.classList.contains('main-nav--closed')) {
+    NAVIGATION_ELEMENT.classList.remove('main-nav--closed');
+    NAVIGATION_ELEMENT.classList.add('main-nav--opened');
+    } else {
+      NAVIGATION_ELEMENT.classList.add('main-nav--closed');
+      NAVIGATION_ELEMENT.classList.remove('main-nav--opened');
+  }
+}
+TOGGLE_NAVIGATION_BUTTON.addEventListener('click', onClickToggleNavigation);
