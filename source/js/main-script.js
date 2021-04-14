305a6e3f344b64a9aea7ee1301a7b36a90e69477
kelter-js <spawnkillerqw@gmail.com>
2021-04-14 08:19:48 +0400

    Реализовал скрипт закрытия модальных окон

--- source/js/main-script.js
+++ source/js/main-script.js
@@ -1,8 +1,15 @@
 const NAVIGATION_ELEMENT = document.querySelector('.main-nav');
 const TOGGLE_NAVIGATION_BUTTON = document.querySelector('.main-nav__toggle');
 
-NAVIGATION_ELEMENT.classList.remove('main-nav--nojs');
+const CLOSE_BUTTONS = [...document.querySelectorAll(".modal__close-modal")];
+const SHOW_MODAL_CLASS = "show-modal";
+const SHOW_MODAL_CLASS_MOD = "show-modal--buy";
 
+CLOSE_BUTTONS.map(button => button.addEventListener('click', function () {
+  this.parentElement.classList.contains(SHOW_MODAL_CLASS) ? this.parentElement.classList.remove(SHOW_MODAL_CLASS) : this.parentElement.classList.remove(SHOW_MODAL_CLASS_MOD)
+}))
+
+NAVIGATION_ELEMENT.classList.remove('main-nav--nojs');
 const onClickToggleNavigation = () => {
   if (NAVIGATION_ELEMENT.classList.contains('main-nav--closed')) {
     NAVIGATION_ELEMENT.classList.remove('main-nav--closed');
@@ -12,4 +19,5 @@ const onClickToggleNavigation = () => {
       NAVIGATION_ELEMENT.classList.remove('main-nav--opened');
   }
 }
+
 TOGGLE_NAVIGATION_BUTTON.addEventListener('click', onClickToggleNavigation);
